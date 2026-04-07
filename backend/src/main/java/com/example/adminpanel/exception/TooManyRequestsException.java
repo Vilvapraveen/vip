2ced39020775d299933bcf65ced002fed9eb019c
VILVA PRAVEEN <113287924+Vilvapraveen@users.noreply.github.com>
2026-04-07 12:38:56 +0530

    Delete backend directory

--- backend/src/main/java/com/example/adminpanel/exception/TooManyRequestsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.adminpanel.exception;
-
-public class TooManyRequestsException extends RuntimeException {
-
-    public TooManyRequestsException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/example/adminpanel/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.adminpanel.repository;
-
-import com.example.adminpanel.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
